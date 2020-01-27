c5dad595214e3fab6daa0c736a83e63e6b83147e
Sasha Savchuk <Sasha>
2020-01-27 02:13:01 +0200

    Updates

---
+++ precache-manifest.945ddfcf1c551c7dacb54f468b21af7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce9fd80fa7b7c9dca53c78b3f407ab1a",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "c1dd750d162239ef08fe",
+    "url": "/goit-react-hw-04-movies/static/css/main.752399e1.chunk.css"
+  },
+  {
+    "revision": "f4cd796ceac70249bd27",
+    "url": "/goit-react-hw-04-movies/static/js/2.cfa3181b.chunk.js"
+  },
+  {
+    "revision": "c1dd750d162239ef08fe",
+    "url": "/goit-react-hw-04-movies/static/js/main.5b286811.chunk.js"
+  },
+  {
+    "revision": "50341c79053ff2fd2719",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.e07164fa.js"
+  }
+]);
